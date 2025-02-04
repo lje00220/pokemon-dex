c00e603d389b9e4601de3e530d29db916c14bdde
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-02-04 11:53:25 +0900

    feat: 로컬스토리지 적용

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { Dashboard } from "../components/Dashboard/Dashboard";
 import { PokemonList } from "../components/PokemonList/PokemonList";
+import { useEffect } from "react";
 
 const Dex = () => {
-  const [selected, setSelected] = useState([]);
+  const [selected, setSelected] = useState(
+    JSON.parse(localStorage.getItem("pokemon")) || []
+  );
+  useEffect(() => {
+    localStorage.setItem("pokemon", JSON.stringify(selected));
+  }, [selected]);
   return (
     <div>
       <Dashboard selected={selected} setSelected={setSelected} />
