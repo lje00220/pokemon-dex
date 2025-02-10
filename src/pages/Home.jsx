bceb4a31534c56a91922e4e7d1347fd078fc8b89
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-02-10 14:27:35 +0900

    Chore: Home.jsx 주석 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,13 @@ import { useNavigate } from "react-router-dom";
 import S from "./Home.style";
 import logoImg from "../assets/images/logo.png";
 
+/**
+ * Home을 출력하는 컴포넌트
+ * - 버튼 클릭 시 Dex 페이지로 이동
+ *
+ * @returns {JSX.Element}
+ */
+
 const Home = () => {
   const navigator = useNavigate();
   return (
